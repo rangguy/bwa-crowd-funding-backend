2920deeed4ee082c12087e0c71db4f86580a7258
rangguy <ranggadwi100@gmail.com>
2024-12-22 23:33:29 +0700

    Gorm mengambil data dari tabel database

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	user "bwastartup/user/users"
 	"fmt"
 
 	"gorm.io/driver/postgres"
@@ -10,10 +11,29 @@ import (
 func main() {
 
 	dsn := "host=localhost user=postgres password=gansboy29 dbname=bwastartup port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
 	}
 	fmt.Println("Connection Success")
-	
+
+	var users []user.User
+	length := len(users)
+
+	fmt.Println(length)
+
+	db.Find(&users)
+
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.ID)
+		fmt.Println(user.Name)
+		fmt.Println(user.Occupation)
+		fmt.Println(user.Email)
+		fmt.Println(user.AvatarFileName)
+		fmt.Println(user.Role)
+		fmt.Println("================")
+	}
 }
