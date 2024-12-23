6894aa614d30b66aaef6191af7679fbfd40aba96
rangguy <ranggadwi100@gmail.com>
2024-12-23 20:49:43 +0700

    Menghasilkan output JSON

--- main.go
+++ main.go
@@ -2,38 +2,53 @@ package main
 
 import (
 	user "bwastartup/user/users"
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
+	//dsn := "host=localhost user=postgres password=gansboy29 dbname=bwastartup port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	//db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	//if err != nil {
+	//	log.Fatal(err.Error())
+	//}
+	//fmt.Println("Connection Success")
+	//
+	//var users []user.User
+	//
+	//db.Find(&users)
+	//
+	//for _, user := range users {
+	//	fmt.Println(user.ID)
+	//	fmt.Println(user.Name)
+	//	fmt.Println(user.Occupation)
+	//	fmt.Println(user.Email)
+	//	fmt.Println(user.AvatarFileName)
+	//	fmt.Println(user.Role)
+	//	fmt.Println("================")
+	//}
 
+	router := gin.Default()
+	router.GET("/handler", handler)
+	err := router.Run()
+	if err != nil {
+		return
+	}
+}
+
+func handler(c *gin.Context) {
 	dsn := "host=localhost user=postgres password=gansboy29 dbname=bwastartup port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
-	fmt.Println("Connection Success")
 
 	var users []user.User
-	length := len(users)
-
-	fmt.Println(length)
-
 	db.Find(&users)
 
-	length = len(users)
-	fmt.Println(length)
-
-	for _, user := range users {
-		fmt.Println(user.ID)
-		fmt.Println(user.Name)
-		fmt.Println(user.Occupation)
-		fmt.Println(user.Email)
-		fmt.Println(user.AvatarFileName)
-		fmt.Println(user.Role)
-		fmt.Println("================")
-	}
+	c.JSON(http.StatusOK, users)
 }
