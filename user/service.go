9503cae7e05f206536ea4d826583b034a58e394c
rangguy <ranggadwi100@gmail.com>
2024-12-27 21:36:28 +0700

    Membuat Service Auth Middleware

--- user/service.go
+++ user/service.go
@@ -10,6 +10,7 @@ type Service interface {
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
 	SaveAvatar(ID int, fileLocation string) (User, error)
+	GetUserByID(ID int) (User, error)
 }
 
 type service struct {
@@ -96,4 +97,17 @@ func (s *service) SaveAvatar(ID int, fileLocation string) (User, error) {
 	}
 
 	return updatedUser, nil
+}
+
+func (s *service) GetUserByID(ID int) (User, error) {
+	user, err := s.repo.FindById(ID)
+	if err != nil {
+		return user, err
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("user not found")
+	}
+
+	return user, nil
 }
