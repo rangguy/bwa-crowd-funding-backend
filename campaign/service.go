b96b9ab6e754cd344e62381a5b72120d78582107
rangguy <ranggadwi100@gmail.com>
2024-12-29 11:22:21 +0700

    Membuat Service GetCampaigns

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,31 @@
 package campaign
+
+type Service interface {
+	GetCampaigns(userID int) ([]Campaign, error)
+}
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{repo}
+}
+
+func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
+	if userID != 0 {
+		campaigns, err := s.repo.FindByUserID(userID)
+		if err != nil {
+			return campaigns, err
+		}
+
+		return campaigns, nil
+	}
+
+	campaigns, err := s.repo.FindAll()
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
