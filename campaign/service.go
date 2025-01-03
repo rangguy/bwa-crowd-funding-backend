a311ad58e7819c2d4ebe562c41869d3918e77952
rangguy <ranggadwi100@gmail.com>
2025-01-03 16:47:48 +0700

    Membuat Service:CreateCampaign bagian 1

--- campaign/service.go
+++ campaign/service.go
@@ -3,6 +3,7 @@ package campaign
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -38,4 +39,24 @@ func (s *service) GetCampaignByID(input GetCampaignDetailInput) (Campaign, error
 	}
 
 	return campaign, nil
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{
+		Name:             input.Name,
+		ShortDescription: input.ShortDescription,
+		Description:      input.Description,
+		GoalAmount:       input.GoalAmount,
+		Perks:            input.Perks,
+		UserID:           input.User.ID,
+	}
+
+	// pembuatan slug
+
+	newCampaign, err := s.repo.Save(campaign)
+	if err != nil {
+		return newCampaign, err
+	}
+
+	return newCampaign, nil
 }
