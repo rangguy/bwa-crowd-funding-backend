40e5908aa25dea22d6e4eb60ec26d325c9cae7da
rangguy <ranggadwi100@gmail.com>
2025-01-06 21:23:00 +0700

    membuat Service GetTransactionsByCampaignID

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,22 @@
 package transaction
+
+type Service interface {
+	GetTransactionsByCampaignID(campaignID int) ([]Transaction, error)
+}
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{repo}
+}
+
+func (s *service) GetTransactionsByCampaignID(campaignID int) ([]Transaction, error) {
+	transactions, err := s.repo.GetByCampaignID(campaignID)
+	if err != nil {
+		return nil, err
+	}
+
+	return transactions, nil
+}
