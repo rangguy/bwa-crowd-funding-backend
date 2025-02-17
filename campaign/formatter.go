611c157e8addf634566613d86b46325bbdb8dc36
rangguy <ranggadwi100@gmail.com>
2025-02-17 23:08:19 +0700

    add backercount at formatter campaign

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -21,6 +21,7 @@ type CampaignDetailFormatter struct {
 	ImageURL         string                   `json:"image_url"`
 	GoalAmount       int                      `json:"goal_amount"`
 	CurrentAmount    int                      `json:"current_amount"`
+	BackerCount      int                      `json:"backer_count"`
 	UserID           int                      `json:"user_id"`
 	Slug             string                   `json:"slug"`
 	Perks            []string                 `json:"perks"`
@@ -76,6 +77,7 @@ func FormatCampaignDetail(campaign Campaign) CampaignDetailFormatter {
 		Description:      campaign.Description,
 		GoalAmount:       campaign.GoalAmount,
 		CurrentAmount:    campaign.CurrentAmount,
+		BackerCount:      campaign.BackerCount,
 		UserID:           campaign.UserID,
 		Slug:             campaign.Slug,
 		ImageURL:         "",
